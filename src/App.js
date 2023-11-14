2b978d137a692b160b68456b9273dae019a80139
eyekavya <kavyasrivastava7830@gmail.com>
2023-11-14 22:15:53 +0530

    managed state for form values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/Auth";
 
 function App() {
   return (
     <>
-      <Auth />
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Auth />}></Route>
+          <Route path="/login" element={<Auth formType="login" />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
